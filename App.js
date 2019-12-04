1db656bd1b10d8037b9a8513dc40eaa618e2c44b
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-12-05 01:11:42 +0200

    Change App to hold tab navigation.

--- App.js
+++ App.js
@@ -1,29 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import MuistinNote from './Components/MuistinNote';
-import MuistinButton from './Components/MuistinButton';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator } from 'react-navigation-tabs';
+ 
+import NotesView from './Screens/NotesView';
 
-export default class App extends React.Component {
-
-  addNote = () => {
-    console.log('App')
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <MuistinButton text='Add note' onClick={this.addNote}/>
-        <MuistinNote/>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const TabNavigator = createBottomTabNavigator (
+  {
+  Home: NotesView,
+  Stats: NotesView,
   },
-});
+  {
+    initialRouteName: 'Home'
+  },
+);
+
+const App = createAppContainer(TabNavigator);
+
+export default App;
