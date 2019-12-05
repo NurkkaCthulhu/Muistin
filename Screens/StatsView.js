123513f05e9d1a121bd1c05ed0787fc5ef5fe1f1
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-12-05 20:01:26 +0200

    Create StatsView.js

---
+++ Screens/StatsView.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View, StatusBar, Text } from 'react-native';
+
+export default class NotesView extends React.Component {
+  static navigationOptions = {
+    title: 'Stats',
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden/>
+        <Text>Some stats should be here.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
