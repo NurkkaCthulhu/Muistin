68a8354bbfc8fafab8b98babe7aff28fd5b0e3b9
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-12-05 20:01:58 +0200

    Add StatsView to tab navigator.

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator } from 'react-navigation-tabs';
  
 import NotesView from './Screens/NotesView';
+import StatsView from './Screens/StatsView';
 
 const TabNavigator = createBottomTabNavigator (
   {
   Home: NotesView,
-  Stats: NotesView,
+  Stats: StatsView,
   },
   {
     initialRouteName: 'Home'
