deda3be19d9a8d939e6e3766e964340894c7f3d2
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-12-15 22:24:41 +0200

    Get soem stats from ASyncStorage.

--- Screens/StatsView.js
+++ Screens/StatsView.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { StyleSheet, View, StatusBar, Text } from 'react-native';
+import { StyleSheet, View, StatusBar, Text, AsyncStorage } from 'react-native';
 
 export default class NotesView extends React.Component {
+
+  state = {keyNumber: 0}
+
+  constructor(props) {
+    super(props);
+    AsyncStorage.getAllKeys()
+        .then((keys) => {this.setState({keyNumber: keys.length})})
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <StatusBar hidden/>
-        <Text>Some stats should be here.</Text>
+        <Text>You have currently this many notes: {this.state.keyNumber}</Text>
       </View>
     );
   }
