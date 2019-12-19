e0a91b41a332f893d113fa1848d6886d869c563a
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-12-20 01:31:38 +0200

    Add button and function for deleting note.

--- Components/MuistinNote.js
+++ Components/MuistinNote.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import MuistinButton from './MuistinButton';
 
 export default class MuistinNote extends React.Component {
 
   state = {title: 'title', body: 'This is a note lol'}
+
+  confirmDelete = () => {
+    Alert.alert(
+      'Delete note',
+      'Are you sure you want to delete this note?',
+      [
+        {text: 'OK', onPress: () => this.props.deletingFunc(this.props.deletingKey)},
+        {text: 'Cancel', onPress: () => console.log('canceled'), style: 'cancel'}
+      ],
+      {cancelable: false},
+    )
+  }
 
   render() {
     return (
         <View style={styles.note}>
           <Text style={styles.title}>{this.props.title}</Text>
           <Text>{this.props.body}</Text>
+          <MuistinButton text='Remove' onClick={this.confirmDelete}/>
         </View>
       );
   }
