6b93f5d5bc42059f5107b70a093e0ef3806d5d99
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-12-20 00:46:32 +0200

    Refactor Note to get it's data from props.

--- Components/MuistinNote.js
+++ Components/MuistinNote.js
@@ -5,29 +5,11 @@ export default class MuistinNote extends React.Component {
 
   state = {title: 'title', body: 'This is a note lol'}
 
-  componentDidMount() {
-    console.log(this.props.storageKey)
-    try {
-      AsyncStorage.getItem(this.props.storageKey)
-        .then((value) => {
-          let note = JSON.parse(value)
-          if (note !== null) {
-            this.setState({title: note.title, body: note.body})
-          } else {
-            this.setState({title: 'Error', body: 'Something went wrong; could not load note information.'})
-          }
-        }
-        )
-    } catch (error) {
-      console.log('Error while loading! ' + error)
-    }
-  }
-
   render() {
     return (
         <View style={styles.note}>
-          <Text style={styles.title}>{this.state.title}</Text>
-          <Text>{this.state.body}</Text>
+          <Text style={styles.title}>{this.props.title}</Text>
+          <Text>{this.props.body}</Text>
         </View>
       );
   }
