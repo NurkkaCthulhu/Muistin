4100b822fd55954c7ec91a5a3e5d745c32978014
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-12-20 01:31:07 +0200

    Sort notes, stretch container (if notes), prepare deleting for notes.

--- Screens/NotesView.js
+++ Screens/NotesView.js
@@ -26,8 +26,12 @@ export default class NotesView extends React.Component {
       let helperArray = []
       result.forEach((note) => {
         let parsedNote = JSON.parse(note[1])
-        helperArray.push({key: note[0], title: parsedNote.title, body: parsedNote.body})
+        let noteKey = note[0].replace(/[^0-9]/g,'')
+        console.log(noteKey)
+        helperArray.push({key: noteKey, title: parsedNote.title, body: parsedNote.body})
       })
+
+      helperArray.sort((a,b) => a.key - b.key)
 
       this.setState({keyNumber: helperArray.length, notes: helperArray})
 
@@ -51,8 +55,8 @@ export default class NotesView extends React.Component {
     }
   }
 
-  deleteNote = () => {
-    console.log('should delete')
+  deleteNote = (id) => {
+    console.log('should delete' + id)
     this.setState({deleteMsg: 'In the future I go to trash.'})
   }
 
@@ -64,7 +68,7 @@ export default class NotesView extends React.Component {
     //AsyncStorage.getAllKeys().then((keys) => console.log(keys))
     console.log(this.state)
     return (
-      <View style={styles.container}>
+      <View style={[styles.container, this.state.notes.length === 0 && styles.centered]}>
         <StatusBar hidden/>
         {this.state.notes.length === 0 ?
           this.renderNoNotes()
@@ -72,7 +76,7 @@ export default class NotesView extends React.Component {
           <FlatList
             data={this.state.notes}
             renderItem={({item}) =>
-              <MuistinNote title={item.title} body={item.body}/>
+              <MuistinNote deletingKey={'key ' + item.key} title={item.title} body={item.body} deletingFunc={this.deleteNote}/>
             }
           />
         }
@@ -86,7 +90,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
+  centered: {
+    alignItems: 'center',
+  }
 });
