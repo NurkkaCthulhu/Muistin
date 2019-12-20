b670b1aea08f56cf8f2cd2a967f6e1cc9b8b7587
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-12-20 14:49:37 +0200

    Refactor note getting, add NavigationEvents to refresh stats.

--- Screens/StatsView.js
+++ Screens/StatsView.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { StyleSheet, View, StatusBar, Text, AsyncStorage } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
 
 export default class NotesView extends React.Component {
 
   state = {keyNumber: 0}
 
-  constructor(props) {
-    super(props);
-    AsyncStorage.getAllKeys()
-        .then((keys) => {this.setState({keyNumber: keys.length})})
+  getAllNotes = async () => {
+    try {
+      AsyncStorage.getAllKeys()
+        .then(keys => this.setState({keyNumber: keys.length}))
+    } catch (error) {
+      console.log('Error while getting notes in Stats tab!' + error)
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
+        <NavigationEvents
+          onWillFocus={() => this.getAllNotes()}
+        />
         <StatusBar hidden/>
         <Text>You have currently this many notes: {this.state.keyNumber}</Text>
       </View>
