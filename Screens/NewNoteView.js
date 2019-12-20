96d73749b9c230069d8db7bd5578783b64940dea
NurkkaCthulhu <nurkkacthulhu@gmail.com>
2019-12-20 16:04:57 +0200

    Add text inputs for title and body of new note.

--- Screens/NewNoteView.js
+++ Screens/NewNoteView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, StatusBar, Text, AsyncStorage } from 'react-native';
+import { StyleSheet, View, StatusBar, Text, TextInput } from 'react-native';
 
 export default class NewNoteView extends React.Component {
 
@@ -7,11 +7,28 @@ export default class NewNoteView extends React.Component {
     title: 'Create a new note',
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {title : '', body: ''}
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <StatusBar hidden/>
         <Text>Welcme to new note</Text>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Title here"
+          onChangeText={(title) => this.setState({title})}
+          value={this.state.title}
+        />
+        <TextInput
+          style={{height: 40}}
+          placeholder="Body here"
+          onChangeText={(body) => this.setState({body})}
+          value={this.state.body}
+        />
       </View>
     );
   }
